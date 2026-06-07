f0804e0422ddbfa9ad059ccbed91ff81dc22f06a
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-07 11:14:55 +0530

    learnt to add exact date and time

--- first.py
+++ first.py
@@ -1,11 +1,14 @@
 # message = "This is python"
 # print(message)
 
+
 # def calculate_prince(price, tax):
 #     return price + tax
 
+
 # x = calculate_prince(100, 40)
 # print(f"total  price is: {x} ")
+
 
 # Getting the exact day and year
 from datetime import datetime
