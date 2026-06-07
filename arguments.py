a2d3b3498324731f988870684e1b73fe3925ba34
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-07 12:39:58 +0530

    learning diff arguments in py :)

--- arguments.py
+++ arguments.py
@@ -1,4 +1,11 @@
-from datetime import datetime
+# from datetime import datetime
 
-x = datetime.now()
-print(x.strftime("%c"))
+# x = datetime.now()
+# print(x.strftime("%X"))
+
+
+def my_func(greetings, *names):
+    for name in names:
+        print(greetings, name)
+
+my_func("Hello", "Sathvk", "Dixit")
