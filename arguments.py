7a27df6e544e9f437d681fdbcf3ca00d8c670f7e
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-07 12:45:18 +0530

    Learned all the diff types of arguments and parameters in python

--- arguments.py
+++ arguments.py
@@ -4,8 +4,17 @@
 # print(x.strftime("%X"))
 
 
+# *args
 def my_func(greetings, *names):
     for name in names:
-        print(greetings, name)
+        print(greetings, names)
 
 my_func("Hello", "Sathvk", "Dixit")
+
+
+
+# **kwargs
+def func(**names):
+    print("His last name is "+names["lname"])
+
+func(fname = "Sathivk", lname = "Dixit")
