f288c3cb167a0f5d830b55c2cb232db75eeb2bf0
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-08 21:22:23 +0530

    Learned converting py txt to json and json to py txt

--- jsonInPython.py
+++ jsonInPython.py
@@ -1,17 +1,30 @@
 import json
 
+# ----------- This code is to convert JSON to python txt ---------------
+
 # some json data
 x = '{"Name": "Sathvk", "Age": 22, "City": "BLY"}'
 
-# parsing data into json
+# parse x
 y = json.loads(x)
 
-# printing json
+# the result is a Python dictionary:
 print(y)
-
-
-
 print()
 
 
 
+# ----------- This code is to convert python txt to JSON ------------
+
+# python txt (dict)
+a = {
+    "name": "Sathvik Dixit",
+    "age": 22,
+    "City": "Bangalore"
+}
+
+# converting into json
+b = json.dumps(a)
+
+# result in JSON formate
+print(b)
