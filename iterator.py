42b69d8b3432f29e062f1bf7f1b68408af29dd53
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-08 20:35:10 +0530

    Understood Iterators in python

--- iterator.py
+++ iterator.py
@@ -1,1 +1,7 @@
 # An iterator is an object that allows you to traverse through sequenceone item at a time
+
+myTuple = ("Apple", "Mango", "Orange")
+myIter = iter(myTuple)
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
