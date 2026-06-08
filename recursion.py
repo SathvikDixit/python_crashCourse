e26597a6a12466280ba0f7451aa9965564e9d5f9
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-08 10:31:47 +0530

    qwerqwer

--- recursion.py
+++ recursion.py
@@ -8,3 +8,14 @@ def recr(n):
         recr(n - 1)
 
 recr(5)
+
+
+
+# Find 7th number of fibonacci sequence
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+    
+print(fibo(7))
