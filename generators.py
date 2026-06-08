bf9691e9fb517b1ed09275c995173632181b61cd
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-08 20:31:49 +0530

    Generators done

--- generators.py
+++ generators.py
@@ -1,6 +1,20 @@
-def func():
-    yield 1
-    yield 2
-    yield 3
-for i in func():
-    print(i)
+# def func():
+#     yield 1
+#     yield 2
+#     yield 3
+# for i in func():
+#     print(i)
+
+
+# A generator is a function that can pause and resume its execution. When a generator function is called it returns a generator object the produces the values one item at a time, on demand insted of storing all the values at once.
+
+
+# this generator prints numbers from 1 to 5
+def myfunc(n):
+    count = 1
+    while count <= n:
+        yield count
+        count += 1
+
+for num in myfunc(5):
+    print(num)
