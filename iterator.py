e759f23f7c518619bd4986be8a19b2bd1c435781
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-08 20:39:38 +0530

    iterators in function

--- iterator.py
+++ iterator.py
@@ -5,3 +5,27 @@ myIter = iter(myTuple)
 print(next(myIter))
 print(next(myIter))
 print(next(myIter))
+
+print()
+
+
+#Another example using a function
+
+class myNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+myclass = myNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
