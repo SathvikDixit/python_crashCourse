dd7b2e47c90b80cc7809f2e84e056fde1a5e7b8d
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-08 21:45:10 +0530

    class and object feature done

--- classANDobject.py
+++ classANDobject.py
@@ -1,9 +1,9 @@
-class myClass:
+class myClass:                                         # Class
     def barking(self):
         print("barking feature")
     
     def eating(self):
         print("Eating feature")
 
-b1 = myClass()
+b1 = myClass()                                         # Object
 b1.barking()
