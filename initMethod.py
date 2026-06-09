dc143c52afec24b31a7758bf6222eab2991eb1a5
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-09 12:19:26 +0530

    learnt using multiple methods in a single class

--- initMethod.py
+++ initMethod.py
@@ -16,3 +16,34 @@ print(b1.name)
 print(b1.age)
 
 print(b1) # printed using __str__ method
+print()
+
+
+
+
+
+# writing a practice code using multiple methods
+class playlist:
+    def __init__(self):
+        # self.name = name
+        self.songs = [ ]
+
+    def add_songs(self, song):
+        self.songs.append(song)
+        print(f"{song} Song Added")
+
+    def remove_song(self, song):
+        if song in self.songs:
+            self.songs.remove(song)
+            print(f"{song} song removed")
+    
+    def show_songs(self):
+        for song in self.songs:
+            print(f"- {song}")
+
+my_playlist = playlist()
+my_playlist.add_songs("Massa Massa")
+my_playlist.add_songs("Helloallo")
+my_playlist.show_songs()
+my_playlist.remove_song("Massa Massa")
+my_playlist.show_songs()
