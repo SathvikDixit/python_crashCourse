3d240e2314627d2aeb7d8a4be2d5f33842a1017b
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-06-09 16:43:09 +0530

    done inheriting stuff from parent class

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class Person:
+    
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+    
+    def printname(self):
+        print(f"First name is: {self.firstname}, Second name is: {self.lastname}")
+    
+x = Person("Sathvik", "Dixit")
+x.printname()
