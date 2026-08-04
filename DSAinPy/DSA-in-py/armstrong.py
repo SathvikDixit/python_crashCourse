c1c1f7b50b31a8d0ca9ca033787067d191e80862
Sathvik Dixit <113917608+SathvikDixit@users.noreply.github.com>
2026-08-04 13:28:55 +0530

    completed armstrong number

--- DSAinPy/DSA-in-py/armstrong.py
+++ DSAinPy/DSA-in-py/armstrong.py
@@ -1,3 +1,6 @@
+# Armstrong Number
+# An Armstrong number is a number that is equal to the sum of its digits each raised to the power of the number of digits 
+
 num = int(input('Enter a number: '))
 original = num
 length = len(str(num))
@@ -9,4 +12,5 @@ while num > 0:
 if original == total:
     print(f'Your number {original} is an Armstrong number')
 else:
-    print(f'Your number {num} is not an Armstrong number')
+    print(f'Your number {original} is not an Armstrong number')
+# Time Complixity = O(log n)        Space Complixity = O(log n)
