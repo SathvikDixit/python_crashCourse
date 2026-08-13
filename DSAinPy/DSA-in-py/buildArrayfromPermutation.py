4b4d7925a4fc4c306b2f3f1e85236b273751130d
tc001-sathvik <b0442505@gmail.com>
2026-08-13 14:28:54 +0530

    New Problem leetcode 1920 Build array in permutation

---
+++ DSAinPy/DSA-in-py/buildArrayfromPermutation.py
@@ -0,0 +1,11 @@
+#Leetcode: 1920
+def buildArrPermut(nums):
+    ans = []
+
+    for i in range(len(nums)):
+        ans.append(nums[nums[i]])
+    return ans
+
+nums = [0, 2, 1, 5, 3, 4]
+result = buildArrPermut(nums)
+print(result)
