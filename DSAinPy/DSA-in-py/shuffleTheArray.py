498776e66bbdff88d949488627c89f471fe038ab
tc001-sathvik <b0442505@gmail.com>
2026-08-13 21:07:03 +0530

    Leetcode 1480 Shuffle the arry

--- DSAinPy/DSA-in-py/shuffleTheArray.py
+++ DSAinPy/DSA-in-py/shuffleTheArray.py
@@ -1,1 +1,11 @@
 #Leetcode 1480
+def ShuffleTheArray(nums, n):
+    ans = []
+    for i in range(n):
+        ans.append(nums[i])
+        ans.append(nums[i+n])
+    return ans
+nums = [1, 2, 3, 4, 5, 6]
+n = 3
+result = ShuffleTheArray(nums, n)
+print(result)
