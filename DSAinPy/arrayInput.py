c1f9c556aa7e6e119ad80fdd33cc4e3a08a91671
tc001-sathvik <b0442505@gmail.com>
2026-08-13 16:14:24 +0530

    array input from the user

---
+++ DSAinPy/arrayInput.py
@@ -0,0 +1,24 @@
+# With fixed size in an array [Integer]:
+''' n = int(input("Enter the size of the array: "))
+print("Now exter the array elements: \n")
+arr =[]
+for i in range (n):
+    arr.append(int(input()))
+print(arr) '''
+
+
+
+
+# With fixed size of array
+
+
+# Without fixed array size [Integer]:
+''' arr = list(map(int, input("Enter numbers use space and press enter after entering numbers: ").split()))
+print(arr)'''
+
+
+
+
+# Without fixe array size [String]:
+arr = input("Enter words: ").split()
+print(arr)
