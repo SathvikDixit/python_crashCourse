5fd6a5b69392222bc02719959e223011e36cd3f8
tc001-sathvik <b0442505@gmail.com>
2026-08-13 12:17:31 +0530

    New Problem leetcode 1929 Concatinate Array

---
+++ DSAinPy/DSA-in-py/concatinateArray.py
@@ -0,0 +1,10 @@
+# Leetcode: 1929
+class Solution():
+    def concatinateArray(self, nums):
+        return nums + nums
+
+nums = [1, 2, 1]
+solution = Solution()
+result = solution.concatinateArray(nums)
+print(result)
+
